5ad377467821192e532d3c7d7e0994545ca1b4d1
prabin1921 <upadhayayaprabin@gmail.com>
2023-12-19 22:18:58 +0545

    Updated

--- Project_Restro/Project_Restro/urls.py
+++ Project_Restro/Project_Restro/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("Restro_App/", include("Restro_App.urls")),
+    
 ]
 
 # urlpatterns = [
